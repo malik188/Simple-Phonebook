36f77f2e96a4812c1355c1b23bbcfce8dc95242b
malik188 <muhammad.malik01@ui.ac.id>
2025-08-04 22:55:35 +0700

    Exercise 2.6-2.10

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,100 @@
-import Course from "./components/Course";
+import { useState } from "react";
+import Person from "./components/Person";
+
+const Filter = ({ filter, onChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={onChange} />
+    </div>
+  );
+};
+
+const PersonForm = ({
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+  onSubmit,
+}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
 
 const App = () => {
-  const course = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
-  return <Course course={course} />;
+  const addContact = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to Phonebook`);
+      return;
+    }
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  // Filter persons case-insensitively
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} onChange={handleFilterChange} />
+      <h2>Add a New Contact</h2>
+      <PersonForm
+        onSubmit={addContact}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <ul>
+        {filteredPersons.map((person) => (
+          <Person key={person.id} person={person} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
