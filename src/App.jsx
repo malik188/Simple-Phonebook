ea0bd75be5ec3f6f03f071bc9313cc11406b97a7
malik188 <muhammad.malik01@ui.ac.id>
2025-08-05 19:40:17 +0700

    Exercise 2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Person from "./components/Person";
+import axios from "axios";
 
 const Filter = ({ filter, onChange }) => {
   return (
@@ -32,15 +33,17 @@ const PersonForm = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    // Fetch initial data from the JSON server
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addContact = (event) => {
     event.preventDefault();
