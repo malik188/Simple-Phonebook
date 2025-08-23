8a8a31a9c0d47b29a5c92117d0ec4e4e7d89316c
malik188 <muhammad.malik@ui.ac.id>
2025-08-23 10:13:31 +0700

    Exercise 3.13-3.14

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const Person = require("./models/person");
 
 app.use(express.json()); // Middleware to parse JSON bodies (required for POST requests)
 app.use(express.static("dist"));
@@ -17,35 +19,22 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons/", (req, res) => {
-  res.json(persons);
+// Endpoint to get all persons
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
+});
+
+// Endpoint to get a specific note by ID
+app.get("/api/persons/:id", (request, response) => {
+  Note.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 app.get("/api/info", (req, res) => {
@@ -55,56 +44,25 @@ app.get("/api/info", (req, res) => {
   res.send(info);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).send({ error: "Note not found" });
-  }
-});
-
-const generateId = () => {
-  return Math.floor(Math.random() * 10000).toString();
-};
-
+// Endpoint to create a new note
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+    return response.status(400).json({ error: "content missing" });
   }
 
-  // Check for duplicate names
-  const existingPerson = persons.find((person) => person.name === body.name);
-  if (existingPerson) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ backend/models/person.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+// Suppress deprecation warning for strictQuery
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+// Define schema to describe the structure of a Person
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+// Format objects returned by mongoose
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
